a5c24e43a81ee51817d808255afa85e089aed414
rc-devs <rc.devs9@gmail.com>
2025-06-12 12:44:01 -0500

    create garage class w/ placeholder methods

---
+++ ruby/02-ruby-basics/ex-enhance-car-mgmt/garage.rb
@@ -0,0 +1,21 @@
+class Garage < Vehicle
+  def add_vehicle
+
+  end
+
+  def remove_class_variable
+
+  end
+
+  def list_all_vehicles
+
+  end
+
+  def find_by_color
+
+  end
+
+  def empty_garage
+    
+  end
+end
