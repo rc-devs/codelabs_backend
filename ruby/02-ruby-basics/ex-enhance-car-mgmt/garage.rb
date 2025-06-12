a5335694d3beaec762f1120ca60ce99727f171cb
rc-devs <rc.devs9@gmail.com>
2025-06-12 14:08:57 -0500

    reference solution; remove/list/find methods

--- ruby/02-ruby-basics/ex-enhance-car-mgmt/garage.rb
+++ ruby/02-ruby-basics/ex-enhance-car-mgmt/garage.rb
@@ -7,16 +7,16 @@ class Garage
     @garage_storage << vehicle #push or insert to array
   end
 
-  def remove_class_variable
-
+  def remove_vehicle
+    @garage_storage.delete_if {|vehicle| vehicle.make == make && vehicle.model == modle} #delete from array if make/model conditions match
   end
 
   def list_all_vehicles
-
+    @garage_storage.map(&:info) #maps over array
   end
 
   def find_by_color
-
+    @garage_storage.select {|vehicle| vehicle.color == color}.map(&:info) #find via condition and map for display
   end
 
   def empty_garage
