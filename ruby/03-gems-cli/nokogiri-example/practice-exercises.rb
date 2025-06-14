c335d69160fa9cf0953d00d61208ae15489a373e
rc-devs <rc.devs9@gmail.com>
2025-06-13 19:58:15 -0500

    httparty and API class

--- ruby/03-gems-cli/nokogiri-example/practice-exercises.rb
+++ ruby/03-gems-cli/nokogiri-example/practice-exercises.rb
@@ -1,5 +1,16 @@
 require "nokogiri"
+require "httparty"
 
 index_doc = Nokogiri::HTML(File.open("index.html"))
 
 index_doc.css("li").each {|li| puts li.text}
+
+class API 
+  def initialize(year)
+    @year
+  end
+
+  def self get_films_by_year(year)
+    @year
+  end
+end
