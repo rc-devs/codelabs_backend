1e89f04ed79163aed4fe9472af234cb24a6cee2c
rc-devs <rc.devs9@gmail.com>
2025-06-13 20:04:18 -0500

    parsed page and films variable from reading

--- ruby/03-gems-cli/nokogiri-example/practice-exercises.rb
+++ ruby/03-gems-cli/nokogiri-example/practice-exercises.rb
@@ -9,5 +9,9 @@ class API
   def self get_films_by_year(year)
     url = "https://en.wikipedia.org/wiki/#{year}_in_film" #url for scrape, string interpolate year for dynamic scrape
     unparsed_page = HTTParty.get(url) #get request page (url variable arg)
+    parsed_page = Nokogiri::HTML(unparsed_page.body) #get url and access html body element content
+
+    #extract the element as nokogiri instances
+    films = parsed_page.css("table.wikitable.sortable tr td:nth-child(2) i a").map {|film| film.text.strip} #iterate over with map
   end
 end
