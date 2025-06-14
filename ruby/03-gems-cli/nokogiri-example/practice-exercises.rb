74e6513eeb9d32563b4078613a2dbc72309e5d20
rc-devs <rc.devs9@gmail.com>
2025-06-13 20:19:58 -0500

    iterate and put each film, correct syntax, successful scrape

--- ruby/03-gems-cli/nokogiri-example/practice-exercises.rb
+++ ruby/03-gems-cli/nokogiri-example/practice-exercises.rb
@@ -6,7 +6,7 @@ index_doc = Nokogiri::HTML(File.open("index.html"))
 index_doc.css("li").each {|li| puts li.text}
 
 class API 
-  def self get_films_by_year(year)
+  def self.get_films_by_year(year)
     url = "https://en.wikipedia.org/wiki/#{year}_in_film" #url for scrape, string interpolate year for dynamic scrape
     unparsed_page = HTTParty.get(url) #get request page (url variable arg)
     parsed_page = Nokogiri::HTML(unparsed_page.body) #get url and access html body element content
@@ -15,5 +15,10 @@ class API
     #table.wikitable.sortable is the element from the html page (table element class)
     #tr td are the html elements table row and table detail; we are digging in to the layers of the html
     films = parsed_page.css("table.wikitable.sortable tr td:nth-child(2) i a").map {|film| film.text.strip} #iterate over with map
+    films.each_with_index do |film, index|
+     puts "#{index + 1}. #{film}" # iterate over and print the index number(+1) and film name
+    end
   end
 end
+
+API.get_films_by_year(2019)
