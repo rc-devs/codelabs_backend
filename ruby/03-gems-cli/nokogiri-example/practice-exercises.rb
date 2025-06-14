54f4272bae1ea0a172bacd8204255fd58dab891b
rc-devs <rc.devs9@gmail.com>
2025-06-13 20:00:23 -0500

    match reading, add notes

--- ruby/03-gems-cli/nokogiri-example/practice-exercises.rb
+++ ruby/03-gems-cli/nokogiri-example/practice-exercises.rb
@@ -6,11 +6,8 @@ index_doc = Nokogiri::HTML(File.open("index.html"))
 index_doc.css("li").each {|li| puts li.text}
 
 class API 
-  def initialize(year)
-    @year
-  end
-
   def self get_films_by_year(year)
-    @year
+    url = "https://en.wikipedia.org/wiki/#{year}_in_film" #url for scrape, string interpolate year for dynamic scrape
+    unparsed_page = HTTParty.get(url) #get request page (url variable arg)
   end
 end
