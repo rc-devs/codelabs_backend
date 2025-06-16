27142841b586a1714eca11fbd2337a2d1f7eb7b5
rc-devs <rc.devs9@gmail.com>
2025-06-16 16:15:46 -0500

    remove old puts

--- ruby/03-gems-cli/countries_of_the_world_cli/lib/cli.rb
+++ ruby/03-gems-cli/countries_of_the_world_cli/lib/cli.rb
@@ -3,10 +3,6 @@ require_relative "scraper.rb"
 class CLI
   def start
     Scraper.scrape_countries
-    puts "Welcome to the Countries of the World CLI!"
-    puts "What is your name?"
-    name = gets.strip
-    puts "Hello #{name}!"
   end
 
   def get_input
