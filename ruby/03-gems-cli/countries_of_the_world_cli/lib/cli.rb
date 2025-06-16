d33ebe5c29900021c315406f1a1505d3c40d2eaa
rc-devs <rc.devs9@gmail.com>
2025-06-16 15:43:08 -0500

    cli class (from reading)

---
+++ ruby/03-gems-cli/countries_of_the_world_cli/lib/cli.rb
@@ -0,0 +1,16 @@
+class CLI
+  def start
+    puts "Welcome to the Countries of the World CLI!"
+    puts "What is your name?"
+    name = gets.strip
+    puts "Hello #{name}!"
+  end
+
+  def get_input
+    gets.strip
+  end
+end
+
+
+puts "Welcome to the Countries of the World CLI!"
+    puts "What is your name?"
