714d1c5b089c749c2c07d57f22dd0f6e327b0687
rc-devs <rc.devs9@gmail.com>
2025-06-16 17:59:10 -0500

    require, put parsed json after nokogiri open

--- ruby/03-gems-cli/cli-class/lib/api.rb
+++ ruby/03-gems-cli/cli-class/lib/api.rb
@@ -1,6 +1,12 @@
+require "nokogiri"
+require "open-uri"
+require "json"
+
 class API
   BASE_URL = "https://www.scrapethissite.com/pages/ajax-javascript/?ajax=true&year="
   def self.find_films_by_year(year)
-    puts 'API'
+    doc = Nokogiri::HTML(URI.open(BASE_URL + year))
+    scraped_movies = JSON.parse(doc.text)
+    puts scraped_movies
   end
 end
