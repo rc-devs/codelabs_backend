0c59d53a4b72606329bdf35b1d78ef922f05e324
rc-devs <rc.devs9@gmail.com>
2025-06-16 16:08:00 -0500

    requires and call scraper

--- ruby/03-gems-cli/countries_of_the_world_cli/lib/cli.rb
+++ ruby/03-gems-cli/countries_of_the_world_cli/lib/cli.rb
@@ -1,5 +1,8 @@
+require_relative "scraper.rb"
+
 class CLI
   def start
+    Scraper.scrape_countries
     puts "Welcome to the Countries of the World CLI!"
     puts "What is your name?"
     name = gets.strip
