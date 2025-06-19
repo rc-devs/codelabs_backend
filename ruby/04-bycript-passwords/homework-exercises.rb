40db31695f2f73389ef4eed287a86f9282adc0a0
rc-devs <rc.devs9@gmail.com>
2025-06-19 15:10:19 -0500

    bcrypt passwords

--- ruby/04-bycript-passwords/homework-exercises.rb
+++ ruby/04-bycript-passwords/homework-exercises.rb
@@ -1,2 +1,13 @@
 #bycript and passwords
+require "bcrypt"
 
+my_password = BCrypt::Password.create("my password")
+
+puts my_password
+
+#outputs 
+#$2a$12$Qyzgy3.oFhzr9P1rxWA6CeeBVblkAs/NI5dbvH3KE4CqLXcp8jjCa
+#$2a$12$ - 'the salt'; makes password more secure; random every time
+
+puts my_password == "my password" #true
+puts my_password == "not my password" #false
