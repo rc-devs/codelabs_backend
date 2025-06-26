1abd6c0bc233321ac88f9dd6b439ec25fc5b9bab
rc-devs <rc.devs9@gmail.com>
2025-06-26 09:28:00 -0500

    normalize addresses

--- 00-ruby/06-database-design/normalization-ex.sql
+++ 00-ruby/06-database-design/normalization-ex.sql
@@ -7,6 +7,16 @@ CREATE TABLE users (
   addresses TEXT
 );
 
+CREATE TABLE addresses(
+  id int PRIMARY KEY,
+  user_id int, 
+  street text,
+  city text, 
+  state text,
+  zip_code text,
+  FOREIGN KEY (user_id) REFERENCES (users.id)
+)
+
 
 CREATE TABLE phone_numbers (
   id int PRIMARY KEY,
