ab5c8595079bf1a34b8a665a2ae3c0e69aa5367d
rc-devs <rc.devs9@gmail.com>
2025-06-26 09:24:31 -0500

    normalize phone number

--- 00-ruby/06-database-design/normalization-ex.sql
+++ 00-ruby/06-database-design/normalization-ex.sql
@@ -4,6 +4,14 @@ CREATE TABLE users (
   last_name TEXT,
   email TEXT,
   password TEXT,
-  addresses TEXT,
-  phone_numbers TEXT
+  addresses TEXT
+);
+
+
+CREATE TABLE phone_numbers (
+  id int PRIMARY KEY,
+  user_id int,
+  phone_type varchar(255)
+  phone_number text,
+  FOREIGN KEY (user_id) REFERENCE (users.id)
 );
